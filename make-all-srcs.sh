6d7c9da1bb589afd1395c91fba32569856537753
Leo Moll <leo.moll@yeasoft.com>
2013-10-17 13:50:40 +0200

    Implemented full fonctionality in make-all-srcs.sh

--- make-all-srcs.sh
+++ make-all-srcs.sh
@@ -1,2 +1,37 @@
 #!/bin/bash
-make -f btsync-common/debian/rules  get-orig-source
+
+set -e
+
+SOURCES="btsync-common btsync"
+
+[ -n "$1" ] && SOURCES="$1"
+
+function enter_build() {
+	pushd $1
+	trap exit_build INT QUIT TERM EXIT
+}
+
+function exit_build() {
+	trap - INT QUIT TERM EXIT
+	popd
+}
+
+for SRCDIR in ${SOURCES}; do
+	if [ -d ${SRCDIR} ]; then
+		enter_build ${SRCDIR}
+		# clean up garbage
+		debuild clean
+		# make sure the orig archive exists
+		debian/rules get-orig-source
+		# make source target
+		debuild -S -sa
+		# cleanup garbage
+		debuild clean
+		# return
+		exit_build
+	else
+		echo ERROR: source tree ${SRCDIR} not found >&2
+	fi
+done
+
+set +e
