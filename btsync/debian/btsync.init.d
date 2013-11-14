8a9450f9ea4059e25006094522502b32d26e5581
Leo Moll <leo.moll@yeasoft.com>
2013-11-14 15:50:52 +0100

    Fixed missing logfile generation broken with new btsync version 1.2.67 (Closes #40)

--- btsync/debian/btsync.init.d
+++ btsync/debian/btsync.init.d
@@ -205,7 +205,7 @@ start_btsync () {
 		--make-pidfile ${CREDENTIALS:+--chuid ${CREDENTIALS}} \
 		--background ${UMASK:+--umask ${UMASK}} \
 		--exec $DAEMON \
-		-- --nodaemon --config $CONFIG_DIR/$CONFFILE $DAEMON_ARGS  || STATUS=1
+		-- --nodaemon --log file --config $CONFIG_DIR/$CONFFILE $DAEMON_ARGS  || STATUS=1
 
 	if [ $STATUS -gt 0 ]; then
 		# start-stop-daemon failed. Let's exit immediately
