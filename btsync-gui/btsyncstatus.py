6e5abe4458c2d4a84a7347c915404fde87153c9c
Leo Moll <leo.moll@yeasoft.com>
2014-04-01 19:32:11 +0200

    Version bump

--- btsync-gui/btsyncstatus.py
+++ btsync-gui/btsyncstatus.py
@@ -35,7 +35,7 @@ from trayindicator import TrayIndicator
 from btsyncapp import BtSyncApp
 from btsyncutils import BtDynamicTimeout
 
-VERSION = '0.8.0'
+VERSION = '0.8.1'
 
 class BtSyncStatus:
 	DISCONNECTED	= 0
